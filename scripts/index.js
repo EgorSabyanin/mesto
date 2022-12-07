c2541d425c61f23164245d5c5a0e7057af2e6baf
Egor Sabyanin <egor.sabyanin.dev@gmail.com>
2022-12-07 22:43:54 +0700

    feat: сделано нажатие на кнопку лайка

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,16 @@
-let editButton = document.querySelector(".profile__edit-button");
-let closeForm = document.querySelector(".popup__close");
+const editButton = document.querySelector(".profile__edit-button");
+const closeForm = document.querySelector(".popup__close");
 
-let userName = document.querySelector(".profile__name");
-let userJob = document.querySelector(".profile__job");
+const userName = document.querySelector(".profile__name");
+const userJob = document.querySelector(".profile__job");
 
-let popup = document.querySelector(".popup");
+const popup = document.querySelector(".popup");
 
-let popupForm = document.querySelector(".popup-form");
+const popupForm = document.querySelector(".popup-form");
 
 // Находим поля формы в DOM
-let nameInput = popupForm.querySelector(".popup-form__input_el_name");
-let jobInput = popupForm.querySelector(".popup-form__input_el_job");
+const nameInput = popupForm.querySelector(".popup-form__input_el_name");
+const jobInput = popupForm.querySelector(".popup-form__input_el_job");
 
 popupForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -29,4 +29,12 @@ editButton.addEventListener("click", function (event) {
 closeForm.addEventListener("click", function (event) {
   event.preventDefault();
   popup.classList.remove("popup_opened");
+});
+
+const cardLikes = document.querySelectorAll(".element__like");
+
+cardLikes.forEach((cardLike) => {
+  cardLike.addEventListener("click", (event) => {
+    cardLike.classList.toggle("element__like_active");
+  });
 });
