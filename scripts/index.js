436a5a7ab401175854ccaa2ba7b329a012f517d5
Egor Sabyanin <egor.sabyanin.dev@gmail.com>
2022-12-07 23:05:51 +0700

    feat: написать начальную загрузку изображений и функцию, которая создаст их

--- scripts/index.js
+++ scripts/index.js
@@ -31,6 +31,64 @@ closeForm.addEventListener("click", function (event) {
   popup.classList.remove("popup_opened");
 });
 
+/**
+ * * Реализация начальной загрузки карточек
+ */
+
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+const cardsContainer = document.querySelector(".elements");
+
+initialCards.forEach((initialCard) => {
+  const cardTemplate = document.querySelector("#element").content;
+  const card = cardTemplate.querySelector(".element").cloneNode(true);
+
+  card.querySelector(".element__image").alt = initialCard.name;
+  card.querySelector(".element__image").src = initialCard.link;
+
+  card.querySelector(".element__text").textContent = initialCard.name;
+
+  cardsContainer.append(card);
+});
+
+function createCards(container, template, name, link) {
+  const card = template.querySelector(".element").cloneNode(true);
+
+  card.querySelector(".element__image").src = link;
+  card.querySelector(".element__image").alt = name;
+  card.querySelector(".element__text").textContent = name;
+
+  container.append(card);
+}
+/**
+ * * Реализация кнопки лайка на карточках
+ */
+
 const cardLikes = document.querySelectorAll(".element__like");
 
 cardLikes.forEach((cardLike) => {
